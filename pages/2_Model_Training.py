7f84acc422ed85dbf7dab4f5f2680b74a2b26595
Muhammad Reihan Pandanarang <reihan@students.unnes.ac.id>
2025-06-17 00:50:23 +0700

    Update 2_Model_Training.py

--- pages/2_Model_Training.py
+++ pages/2_Model_Training.py
@@ -76,6 +76,6 @@ if df is not None and 'Inches' in df.columns:
 
             st.success("🎉 Model berhasil dilatih dan disimpan!")
             st.metric(label="**R-squared (R²)**", value=f"{r2:.2f}")
-            st.metric(label="**Mean Absolute Error (MAE)**", value=f"€{mae:.2f}")
+            st.metric(label="**Mean Absolute Error (MAE)**", value=f"Juta{mae:.2f}")
 else:
-    st.error("Gagal memuat data atau membuat fitur 'Inches'. Pastikan file 'laptop_prices.csv' benar dan memiliki kolom 'ScreenResolution'. Pelatihan tidak dapat dilanjutkan.")
+    st.error("Gagal memuat data")
